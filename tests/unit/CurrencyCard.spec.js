7a1daa978f3bde724ec35286ff8f40e0ab2d06fa
thi174hcmus@gmail.com <thi174hcmus@gmail.com>
2021-06-12 21:52:13 +0700

    Fix

--- tests/unit/CurrencyCard.spec.js
+++ tests/unit/CurrencyCard.spec.js
@@ -7,7 +7,7 @@ describe('CurrencyCard.vue', () => {
     const ticker = { price: 1, percent: 10, chg: 100, vol: 1000 }
     const info = { base: '', name: '', quote: '', symbol: '' }
     const wrapper = shallowMount(CurrencyCard, {
-      propsData: { ticker, in }
+      propsData: { ticker, info }
     })
     expect(wrapper.props().ticker).to.equal(ticker)
     expect(wrapper.props().info).to.equal(info)
